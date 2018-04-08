2efc60839fc29657279e2567a34a756930fa6bfa
王涵毅 <henrywang@wanghanyis-MacBook-Pro.local>
2018-04-08 19:44:13 +0800

    Test Jieba

--- hello/views.py
+++ hello/views.py
@@ -3,11 +3,16 @@ from django.http import HttpResponse
 
 from .models import Greeting
 import requests
+import jieba.analyse
 
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    #r = requests.get('http://httpbin.org/status/418')
+    tags = jieba.analyse.extract_tags(
+        '今天的天氣非常好，適合到台灣大學走走，晚上吃拉麵',
+        topK=topK, withWeight=False)
+
+    print(tags)
+    return HttpResponse('<pre>' + tags + '</pre>')
 
 # Create your views here.
 # def index(request):
