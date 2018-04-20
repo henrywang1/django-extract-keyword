b669ad7f997d9d0417cb1e2049719159ba8dffa1
王涵毅 <wanghy917@gmail.com>
2018-04-20 17:22:54 +0800

    temp

--- extract/models.py
+++ extract/models.py
@@ -5,13 +5,11 @@ from django.utils import timezone
 
 class Keyword(models.Model):
     keyword  = models.CharField(max_length=30)
-    keyword2  = models.CharField(max_length=30)
     def __str__(self):
         return self.keyword
 
 class StopWord(models.Model):
     stop_word  = models.CharField(max_length=30)
-    keyword2  = models.CharField(max_length=30)
     def __str__(self):
         return self.stop_word
         
