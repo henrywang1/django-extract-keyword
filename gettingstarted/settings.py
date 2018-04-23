67d48bda3e3c061ea803d2ea442882c5747fc625
王涵毅 <wanghy917@gmail.com>
2018-04-23 10:39:08 +0800

    update UI

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'extract_local',                      
+        # 'USER': 'db_user',
+        # 'PASSWORD': 'db_user_password',
+        # 'HOST': '',
+        'PORT': 5432,
     }
 }
 
