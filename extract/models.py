6478af2bdcbb9296d79b380fd4ebc20f4af9984f
王涵毅 <wanghy917@gmail.com>
2018-04-26 23:30:13 +0800

    word2vec

--- extract/models.py
+++ extract/models.py
@@ -12,6 +12,13 @@ class StopWord(models.Model):
     stop_word  = models.CharField(max_length=30, unique=True)
     def __str__(self):
         return self.stop_word
+
+class Word2Vec(models.Model):
+    base  = models.BinaryField()
+    syn0  = models.BinaryField()
+    syn1neg  = models.BinaryField()
+    def __str__(self):
+        return '[binary data]'
         
 
 
--- extract/trend.py
+++ extract/trend.py
@@ -1,80 +1,107 @@
 from collections import Counter
 from collections import defaultdict
 import pytrends
+import pytrends
 from pytrends.request import TrendReq
 pytrends = TrendReq(hl='zh-tw', tz=360)
+import pickle
+import gensim
+from sklearn import cluster
+import math
 
-def getTopKRelated(keyword, k=4):
+# c.execute('''SELECT a FROM images''')
+# mview = c.fetchone()
+# new_bin_data = bytes(mview)
+# print(new_bin_data)
+
+with open ('./extract/cluster_dict.pickle', 'rb') as f:
+    cluster_dict = pickle.load(f)
+
+model = gensim.models.Word2Vec.load('./extract/skip-gram-mc1') 
+dict_relate = {}
+
+def get_cluster(input_list):
+    NUM_CLUSTERS = math.ceil(len(input_list)/5)
+    input_list = [i for i in input_list if i in cluster_dict]
+    kmeans = cluster.KMeans(n_clusters=NUM_CLUSTERS)
+    X = model[input_list]
+    kmeans.fit(X)
+    labels = kmeans.labels_
+
+    final_list = [[] for i in range(NUM_CLUSTERS)]
+    for idx, item in enumerate(input_list):
+        gp = labels[idx]
+        final_list[gp].append(item)
+    return final_list
+
+def get_topk_related(keyword, k=3):
+    print(keyword)
+    print(dict_relate)
+    if keyword in dict_relate:
+        return dict_relate[keyword]
     print('keyword={0}'.format(keyword))
     pytrends.build_payload([keyword], cat=0, timeframe='today 1-m', geo='', gprop='')
     ret = list(pytrends.related_topics().values())[0]
-    return set(ret.title[0:k].values) if ret is not None else None
+    ret = set(ret.title[0:k].values) if ret is not None else None
+    dict_relate[keyword] = ret
+    return ret
 
-def extend_result(item_list, dict_suggest, dict_relate):
-    all_relate = [list(dict_relate[key]) for key in item_list if dict_relate[key]]
-    all_relate = [ll for l in all_relate for ll in l if len(ll)>1]
+def extend_result(item_list): 
+    if len(item_list) >=5:
+        return item_list
+    print('item_list=')
+    print(item_list)
+    # for item in item_list:
+    #     print(item)
+    all_relate = [get_topk_related(item) for item in item_list]
+    all_relate = [item for sublist in all_relate if sublist for item in sublist]
     all_relate = Counter(all_relate).most_common()
     all_relate = [item[0] for item in all_relate if item[0] not in item_list]
-    return (item_list+all_relate)[:5]
+    length = (len(item_list)+1)*2
+    return (item_list+all_relate)[:length]
 
-def word_related(the_list):
-    ret = [] #這個只是拿來看兩兩關係
-    dict_suggest = {}
-    dict_relate = {}
-    for i in the_list:
-        suggestions = []
-        topRelated = dict_relate.get(i)
-        if topRelated is None: 
-            topRelated = dict_relate[i] = getTopKRelated(i, 3)
-
-        suggestions = dict_suggest.get(i)
-        if suggestions is None: 
-            suggestions = dict_suggest[i] = set([s['title'] for s in pytrends.suggestions(i)][:3])       
-        suggestions_or_related = suggestions | topRelated if topRelated else suggestions
-        ret.append((i, suggestions_or_related))
-
-    group = []
-    dict_word = {} 
-    for i, item in enumerate(ret):
-        if item[0] in dict_word: #有組了
-            continue
-
-        candidate = []
-        related_words = []
-        found = False
-        for successor in ret[i+1:]:
-            #找後面的 intersction 看有沒有有組的
-            intersection = (item[1].intersection(successor[1]))
-            if intersection:
-                if successor[0] in dict_word: #.get(successor[0]) is not None: #這個對象有組別了 就跟他一組
-                    idx = dict_word.get(successor[0])
-                    group[idx].append(item[0])
-                    related_words[idx].append(intersection)
-                    dict_word[item[0]] = idx
-                    found=True
-                    break
-                else:
-                    candidate.append(successor[0])
-        if not found:
-            candidate = [item[0]] + candidate
-            group.append(candidate)
-            related_words.append(item[1])
-            for c in candidate:
-                dict_word[c] = (len(group)-1)
-    
-    
-    v = defaultdict(list)
-    for key, value in sorted(dict_word.items()):
-        v[value].append(key)
-    result = list(v.values())
-    result = [extend_result(r, dict_suggest, dict_relate) for r in result]
+def get_trend_result(result):
     result_list = []
     for r in result:
         if len(r)<2:
             continue
-        pytrends.build_payload(r, cat=0, timeframe='today 1-m')
-        interest_over_time_df = pytrends.interest_over_time()
-        interest_over_time_df = interest_over_time_df.mean()[:-1].sort_values(ascending=False)
-        interest_over_time_df = interest_over_time_df.map(lambda x: '%2.1f' % x)
-        result_list.append(interest_over_time_df.to_dict())
+        length = len(r)
+        if length <= 5:   
+            
+            pytrends.build_payload(r, cat=0, timeframe='today 1-m')
+            interest_over_time_df = pytrends.interest_over_time()
+            interest_over_time_df = interest_over_time_df.mean()[:-1]
+            interest_over_time_df = interest_over_time_df.map(lambda x: '%d' % round(x))
+            interest_over_time_df = interest_over_time_df.to_dict()
+            result_list.append(interest_over_time_df)
+        else:
+            l_r = r[0:length//2][:5]
+            r_r = r[length//2-1:][:5]
+            pytrends.build_payload(l_r, cat=0, timeframe='today 1-m')
+            df_1 = pytrends.interest_over_time()
+            df_1 = df_1.mean()[:-1]            
+            pytrends.build_payload(r_r, cat=0, timeframe='today 1-m')
+            df_2 = pytrends.interest_over_time()
+            df_2 = df_2.mean()[:-1]
+            
+            if(df_1[-1] > 0 and df_2[0] >0):
+                r1 = df_1/df_1[-1]
+                r2 = df_2/df_2[0]
+            else:
+                r1=df_1
+                r2=df_2
+            interest_over_time_df = pd.concat([r1, r2])
+            interest_over_time_df = interest_over_time_df.to_dict()
+            result_list.append(interest_over_time_df)
+
+    return result_list
+
+def word_related(tags):
+    tag_cluster = get_cluster(tags)
+    print('tag_cluster=')
+    print(tag_cluster)
+    cluster_result = [extend_result(r) for r in tag_cluster]
+    # cluster_result = extend_result(cluster_result)
+    result_list = get_trend_result(cluster_result)
+    print(result_list)
     return result_list
